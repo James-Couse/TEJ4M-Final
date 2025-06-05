b236f6aa50680497c4cd72719b95d51d58d69464
James Couse <144252195+James-Couse@users.noreply.github.com>
2025-06-05 13:08:46 +0000

    mastey might be done

--- Masterwip.ino
+++ Masterwip.ino
@@ -30,33 +30,87 @@ int counter(int addsub) {
 	Wire.beginTransmission(9)
   	if (addsub < 2) {
     	Serial.println(addsub);
-     	for (int EightA = 0; twosA <= 1; ++twosA) { // 2A
-       		digitalWrite(DIGIT_A2, twosA);
-       		for (int EightB = 0; twosB <= 1; ++twosB) { // 2B
+     	for (int EightA = 0; EightA <= 1; ++EightA) { // 2A
+       		digitalWrite(DIGIT_A2, EightA);
+       		for (int EightB = 0; EightB <= 1; ++EightB) { // 2B
          		if (EightB == 0) {
-					Wire.write(128);
+					Wire.write(1280);
 				}
 				if (EightB == 1) {
-					Wire.write(129);
+					Wire.write(1281);
 				}
-          		for (int SevenA = 0; onesA <= 1; ++onesA) { // 1A
-            		digitalWrite(DIGIT_A1, onesA);
-            		for (int SevenB = 0; onesB <= 1; ++onesB) { // 1B
+          		for (int SevenA = 0; SevenA <= 1; ++onSevenAesA) { // 1A
+            		digitalWrite(DIGIT_A1, SevenA);
+            		for (int SevenB = 0; SevenB <= 1; ++SevenB) { // 1B
               			if (SevenB == 0) {
-							Wire.write(64);
+							Wire.write(640);
 						}
 						if (SevenB == 1) {
-							Wire.write(65);
+							Wire.write(641);
 						}
-						for (int SixA = 0; twosA <= 1; ++twosA) { // 2A
-       						digitalWrite(DIGIT_A2, twosA);
-       						for (int SixB = 0; twosB <= 1; ++twosB) { // 2B
-         						digitalWrite(DIGIT_B2, twosB);
-          						for (int FiveA = 0; onesA <= 1; ++onesA) { // 1A
-            						digitalWrite(DIGIT_A1, onesA);
-            						for (int FiveB = 0; onesB <= 1; ++onesB) { // 1B
-              							digitalWrite(DIGIT_B1, onesB);
-              						delay(500);
+						for (int SixA = 0; SixA <= 1; ++SixA) { // 2A
+       						digitalWrite(DIGIT_A2, SixA);
+       						for (int SixB = 0; SixB <= 1; ++SixB) { // 2B
+         						if (SixB == 0) {
+									Wire.write(320);
+								}
+								if (SixB == 1) {
+									Wire.write(321);
+								}
+          						for (int FiveA = 0; FiveA <= 1; ++FiveA) { // 1A
+            						digitalWrite(DIGIT_A1, FiveA);
+            						for (int FiveB = 0; FiveB <= 1; ++FiveB) { // 1B
+              							if (FiveB == 0) {
+											Wire.write(160);
+										}
+										if (FiveB == 1) {
+											Wire.write(161);
+										}
+										for (int FourA = 0; FourA <= 1; ++FourA) { // 2A
+       										digitalWrite(DIGIT_A2, FourA);
+       										for (int FourB = 0; FourB <= 1; ++FourB) { // 2B
+         										if (FourB == 0) {
+													Wire.write(80);
+												}
+												if (FourB == 1) {
+													Wire.write(81);
+												}
+												for (int ThreeA = 0; ThreeA <= 1; ++ThreeA) { // 1A
+													digitalWrite(DIGIT_A1, ThreeA);
+													for (int ThreeB = 0; ThreeB <= 1; ++ThreeB) { // 1B
+														if (ThreeB == 0) {
+															Wire.write(40);
+														}
+														if (ThreeB == 1) {
+															Wire.write(41);
+														}
+														for (int TwoA = 0; TwoA <= 1; ++TwoA) { // 2A
+															digitalWrite(DIGIT_A2, TwoA);
+															for (int TwoB = 0; TwoB <= 1; ++TwoB) { // 2B
+																if (TwoB == 0) {
+																	Wire.write(20);
+																}
+																if (TwoB == 1) {
+																	Wire.write(21);
+																}
+																for (int OneA = 0; OneA <= 1; ++OneA) { // 1A
+																	digitalWrite(DIGIT_A1, OneA);
+																	for (int OneB = 0; OneB <= 1; ++OneB) { // 1B
+																		if (OneB == 0) {
+																			Wire.write(10);
+																		}
+																		if (OneB == 1) {
+																			Wire.write(11);
+																		}
+              														delay(500);
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
             						}
           						}
         					}
