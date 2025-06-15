ed591ef0d9b73d1808a02e2e6851c0dc5a2a2157
James Couse <144252195+James-Couse@users.noreply.github.com>
2025-06-15 18:06:51 +0000

    still not wokring

--- SlaveWIP.ino
+++ SlaveWIP.ino
@@ -37,40 +37,56 @@ void receiveEvent(int bytes) {
   x = Wire.read();
   Serial.println(x);
   Serial.println("---");
-}
-void loop() {
   switch (x) {
       case 1281:
-          digitalWrite(DIGIT_B8, HIGH);
+          digitalWrite(DIGIT_B8, 1);
+    	  x = 0;
       case 1280:
-          digitalWrite(DIGIT_B8, LOW);
+          digitalWrite(DIGIT_B8, 0);
+      	  x = 0;
       case 641:
-          digitalWrite(DIGIT_B7, HIGH);
+          digitalWrite(DIGIT_B7, 1);
+    	  x = 0;
       case 640:
-          digitalWrite(DIGIT_B7, LOW);
+          digitalWrite(DIGIT_B7, 0);
+    	  x = 0;
       case 321:
-          digitalWrite(DIGIT_B6, HIGH);
+          digitalWrite(DIGIT_B6, 1);
+      	  x = 0;
       case 320:
-          digitalWrite(DIGIT_B6, LOW);
+          digitalWrite(DIGIT_B6, 0);
+    	  x = 0;
       case 161:
-          digitalWrite(DIGIT_B5, HIGH);
+          digitalWrite(DIGIT_B5, 1);
+      	  x = 0;
       case 160:
-          digitalWrite(DIGIT_B5, LOW);
+          digitalWrite(DIGIT_B5, 0);
+    	  x = 0;
       case 81:
-          digitalWrite(DIGIT_B4, HIGH);
+          digitalWrite(DIGIT_B4, 1);
+      	  x = 0;
       case 80:
-          digitalWrite(DIGIT_B4, LOW);
+          digitalWrite(DIGIT_B4, 0);
+    	  x = 0;
       case 41:
-          digitalWrite(DIGIT_B3, HIGH);
+          digitalWrite(DIGIT_B3, 1);
+    	  x = 0;
       case 40:
-          digitalWrite(DIGIT_B3, LOW);
+          digitalWrite(DIGIT_B3, 0);
+    	  x = 0;
       case 21:
-          digitalWrite(DIGIT_B2, HIGH);
+          digitalWrite(DIGIT_B2, 1);
+    	  x = 0;
       case 20:
-          digitalWrite(DIGIT_B2, LOW);
+          digitalWrite(DIGIT_B2, 0);
+    	  x = 0;
       case 11:
-          digitalWrite(DIGIT_B1, HIGH);
+          digitalWrite(DIGIT_B1, 1);
+    	  x = 0;
       case 10:
-          digitalWrite(DIGIT_B1, LOW);
+          digitalWrite(DIGIT_B1, 0);
+    	  x = 0;
     }
+}
+void loop() {
 }
